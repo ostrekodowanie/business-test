640802de7dd04bdb19cf89604568bf164e048a15
Filip <filfer05@gmail.com>
2022-04-24 00:46:49 +0200

    button hover

--- scripts/h1line.js
+++ scripts/h1line.js
@@ -1,18 +1,10 @@
 const title = document.querySelector('.s1 h1');
-const form = document.querySelector('.email-input')
-const defaultLineHeight = () => title.style.lineHeight = "130%";
+const form = document.querySelector('.email-input');
 const defaultFormMargin = () => form.style.marginTop = "3rem";
-defaultLineHeight();
 defaultFormMargin();
 
 window.addEventListener('resize', () => {
     let titleWidth = title.offsetWidth;
-    if(titleWidth < 317) {
-        title.style.lineHeight = "150%";
-        form.style.marginTop = "2rem";
-    }
-    else {
-        defaultLineHeight();
-        defaultFormMargin();
-    };
+    if(titleWidth < 317) form.style.marginTop = "2rem";
+    else defaultFormMargin();
 });
