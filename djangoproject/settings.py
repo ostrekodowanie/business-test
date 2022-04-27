e4fe33ac72fc6a6d5e1470e4f08781a48a92de10
Sebastian <se6359@gmail.com>
2022-04-27 23:29:47 +0200

    heroku

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ulo8xdng3#z1lxmj3h0#0a$-x)(v(5#qk)vc293r7lg%&@m#^j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','https://business-test1.herokuapp.com']
 
--- djangoproject/urls.py
+++ djangoproject/urls.py
@@ -17,16 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from djangoapp.views import home_view
 
-from django.conf import settings
-from django.views.static import serve
-from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name='home'),
-    
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
 
